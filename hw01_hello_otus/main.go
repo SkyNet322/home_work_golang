bf944981005d802a16995fce752e7ce42498d769
papazyan_a_v <Wor!dofwarcraft3>
2022-09-06 22:09:39 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	data := "Hello, OTUS!"
+
+	result := stringutil.Reverse(data)
+
+	fmt.Println(result)
 }
